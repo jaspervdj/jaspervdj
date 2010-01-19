3c50fd45ff063ea25f8a96c3192fa10752e6d8ae
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-01-19 10:31:29 +0100

    Move to trunk version of hakyll.

--- hakyll.hs
+++ hakyll.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Text.Hakyll (hakyll)
+import Text.Hakyll (hakyll, defaultHakyllConfiguration)
 import Text.Hakyll.Render
 import Text.Hakyll.Util (trim, link)
 import Text.Hakyll.File (getRecursiveContents, directory, removeSpaces)
@@ -10,57 +10,58 @@ import Text.Hakyll.Context (renderDate, renderValue, ContextManipulation)
 import qualified Data.Map as M
 import Data.List (sort, intercalate)
 import Control.Monad (liftM, mapM_)
+import Control.Monad.Reader (liftIO)
 import Data.Either (Either(..))
 
-main = hakyll $ do
-    putStrLn "Copying static directories and compressing css..."
+main = hakyll defaultHakyllConfiguration $ do
+    liftIO $ putStrLn "Copying static directories and compressing css..."
     directory static "images"
     directory static "js"
     directory css "css"
     static "favicon.ico"
 
-    putStrLn "Finding posts..."
+    liftIO $ putStrLn "Finding posts..."
     postPaths <- liftM (reverse . sort) $ getRecursiveContents "posts"
     let renderablePosts = map createPagePath postPaths
 
-    putStrLn "Getting tags..."
+    liftIO $ putStrLn "Getting tags..."
     tagMap <- readTagMap postPaths
 
-    putStrLn "Generating index..."
+    liftIO $ putStrLn "Generating index..."
     let recentPosts = renderAndConcatWith postManipulation
-                                          "templates/postitem.html"
+                                          ["templates/postitem.html"]
                                           (take 3 renderablePosts)
     renderChain ["index.html", "templates/default.html"] $
         createCustomPage "index.html" ("templates/postitem.html" : postPaths)
             [("title", Left "Home"), ("posts", Right recentPosts),
              ("tagcloud", Left $ renderTagCloud tagMap tagToURL 100 120)]
 
-    putStrLn "Generating rss feed..."
+    liftIO $ putStrLn "Generating rss feed..."
     let recentItems = renderAndConcatWith postManipulation
-                                          "templates/rssitem.xml"
+                                          ["templates/rssitem.xml"]
                                           (take 5 renderablePosts)
     renderChain ["templates/rss.xml"] $
         createCustomPage "rss.xml" ("templates/rssitem.xml" : postPaths) [("items", Right recentItems)]
 
-    putStrLn "Generating general post list..."
+    liftIO $ putStrLn "Generating general post list..."
     renderPostList "posts.html" "All posts" postPaths
 
-    putStrLn "Generating all posts..."
+    liftIO $ putStrLn "Generating all posts..."
     mapM_ (renderChainWith postManipulation ["templates/post.html", "templates/default.html"])
           renderablePosts
 
-    putStrLn "Creating tag post lists..."
+    liftIO $ putStrLn "Creating tag post lists..."
     mapM_ (\(t, p) -> renderPostList (tagToURL t)
                         ("Posts tagged " ++ t) (sort $ reverse p)) $ M.toList tagMap
 
-    putStrLn "Generating simple pages..."
+    liftIO $ putStrLn "Generating simple pages..."
     mapM_ (renderChain ["templates/default.html"] . createPagePath)
             [ "contact.markdown"
             , "projects.markdown"
             , "404.html"
             ]
 
-    putStrLn "Succes!"
+    liftIO $ putStrLn "Succes!"
 
 tagToURL :: String -> String
 tagToURL tag = "$root/tags/" ++ (removeSpaces tag) ++ ".html"
@@ -70,8 +71,8 @@ postManipulation = renderTagLinks tagToURL
                  . renderDate "prettydate" "%B %e, %Y" "Date unknown"
 
 renderPostList url title posts = do
-    putStrLn $ "Generating post list " ++ title ++ "..."
-    let postItems = renderAndConcatWith postManipulation "templates/postitem.html" $ map createPagePath posts
+    liftIO $ putStrLn $ "Generating post list " ++ title ++ "..."
+    let postItems = renderAndConcatWith postManipulation ["templates/postitem.html"] $ map createPagePath posts
     renderChain ["posts.html", "templates/default.html"] $
         createCustomPage url ("templates/postitem.html" : posts)
         [("title", Left title), ("posts", Right postItems)]
