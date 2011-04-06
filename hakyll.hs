3c42026cd55394093f02fd5c18e7d72a4009f356
Jasper Van der Jeugt <jaspervdj@gmail.com>
2011-04-06 09:28:39 +0200

    Update tagmap thingy

--- hakyll.hs
+++ hakyll.hs
@@ -76,7 +76,7 @@ main = hakyll $ do
     -- Add a tag list compiler for every tag
     match "tags/*" $ route $ setExtension ".html"
     metaCompile $ require_ "tags"
-        >>> arr (M.toList . tagsMap)
+        >>> arr tagsMap
         >>> arr (map (\(t, p) -> (tagIdentifier t, makeTagList t p)))
 
     -- Read templates
